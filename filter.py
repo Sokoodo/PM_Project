5876117f232837ad2a6ed6ccb0d4d86a72e74d87
903705 Santarelli Francesco <903705@edu.p.lodz.pl>
2022-05-25 17:52:31 +0200

    Make the program more readable

--- filter.py
+++ filter.py
@@ -26,7 +26,6 @@ print('Log size =' + str(len(log)))
 log = start_activities_filter.apply_auto_filter(log,
                                                 parameters={start_activities_filter.Parameters.DECREASING_FACTOR: 0.5})
 end_activities = end_activities_filter.get_end_activities(log)
-
 log = end_activities_filter.apply_auto_filter(log, parameters={end_activities_filter.Parameters.DECREASING_FACTOR: 0.5})
 print('Log size =' + str(len(log)))
 
@@ -36,8 +35,7 @@ for trace in log:
         event["customClassifier"] = event["concept:name"] + event["lifecycle:transition"]
 
 # Discovering with the Heuristic Miner
-log_heuristic = log
-heu_net = heuristics_miner.apply_heu(log_heuristic, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
+heu_net = heuristics_miner.apply_heu(log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
 
 # Visualizing the Heuristic Net
 gviz = hn_visualizer.apply(heu_net)
@@ -49,7 +47,6 @@ gviz = pn_visualizer.apply(net, im, fm)
 pn_visualizer.view(gviz)
 pnml_exporter.apply(net, im, "petri_from_heuristic.pnml", final_marking=fm)
 
-'''
 # Discovering with the Alpha Miner
 parameters = {alpha_miner.Variants.ALPHA_VERSION_CLASSIC.value.Parameters.ACTIVITY_KEY: "customClassifier"}
 net, initial_marking, final_marking = alpha_miner.apply(log, parameters=parameters)
@@ -60,4 +57,4 @@ pn_visualizer.view(gviz)
 
 # Exporting a .pnml file with the resulting PetriNet
 pnml_exporter.apply(net, initial_marking, "petri_final.pnml", final_marking=final_marking)
-'''
+
